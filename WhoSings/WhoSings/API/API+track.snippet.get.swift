e19bc7d093f1aff526bcaa70a00be7fdd25303e0
Matteo Vitolo <matteo.vitolo@moneyfarm.com>
2021-02-13 12:58:19 +0100

    minor fixes

---
+++ WhoSings/WhoSings/API/API+track.snippet.get.swift
@@ -0,0 +1,8 @@
+//
+//  API+track.snippet.get.swift
+//  WhoSings
+//
+//  Created by Matteo Vitolo on 13/02/2021.
+//
+
+import Foundation
---
+++ WhoSings/WhoSings/Model/Snippet.swift
@@ -0,0 +1,8 @@
+//
+//  Snippet.swift
+//  WhoSings
+//
+//  Created by Matteo Vitolo on 13/02/2021.
+//
+
+import Foundation
